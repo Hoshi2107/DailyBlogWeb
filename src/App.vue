ab1faae82c9c75d0ff3080f2ce7f0e832bc69a79
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2026-01-12 18:53:12 +0700

    add navbar(v1), new homepage interface

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,4 @@ import router from "./router";
   <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid px-4">
+      <router-link class="navbar-brand fw-bold" to="/">
+        📝 DailyBlog
+      </router-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navMenu"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navMenu">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-2">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/posts">Posts</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/create-post">
+              Write
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-light btn-sm" to="/login">
+              Login
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+<style scoped></style>
