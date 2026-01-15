0a230aae2c90e6e9f146ed18cafcb9dee7a09095
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2026-01-15 19:26:57 +0700

    delete temp old posts route

--- src/router/Index.js
+++ src/router/Index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DailyBlogHomePage from "../components/DailyBlogHomePage.vue";
-import Posts from "../components/Posts.vue";
 import Login from "../components/Login.vue";
 import Account from "../components/Account.vue";
 import Register from "../components/Register.vue";
@@ -12,11 +11,6 @@ const routes = [
     path: "/",
     name: "Home",
     component: DailyBlogHomePage,
-  },
-  {
-    path: "/posts",
-    name: "Posts",
-    component: Posts,
   },
   {
     path: "/login",
